4af6739dc9712c3c0c4852b551eabe0269213f91
reijirokanesaka <reijiro.kanesaka@enisias.com>
2023-05-26 14:59:33 +0900

    rejirou commit

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -9,6 +9,7 @@ public class TestController {
     @GetMapping(path = "/test")
     public String getTest(HttpServletRequest request) {
         System.out.println("haro-");
+        System.out.println("通ってくれぇい");
     	return "Hello World!";
     }
 }
